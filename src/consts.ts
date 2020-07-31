2b57a7ca853e8e26f208b61bc879b2aaf528a18f
Vinicius Jose Fritzen <www.vini84200@hotmail.com>
2020-07-31 15:01:27 -0300

    Ip de teste

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const CONNECTION_STRING = "localhost:550"
+export const CONNECTION_STRING = "localhost:8100"
 // Messege Events
 export const ALL_MESSEGES = "chatroom:all-messages"
 export const SEND_MESSEGE = "chatroom:send-message"
